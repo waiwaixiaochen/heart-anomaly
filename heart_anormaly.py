86b249fd8ed6260a284b67a93f3c9fe133c0d0b4
waiwaixiaochen <33296941+waiwaixiaochen@users.noreply.github.com>
2019-11-16 21:08:05 -0800

    added second learner

--- heart_anormaly.py
+++ heart_anormaly.py
@@ -36,7 +36,7 @@ def prepare_basic_data(train_y, train_row_num, train_x):
 # preprocess the data for Naive Bayesian Learning
 def prepare_data_naive(p_normal, train_normal, count1):
     
-    # create four lists to record the probabilities
+    # create  lists to record the probabilities
     # one list for p(xi=1|normal), one list for p(xi=0|normal)
     # one list for p(xi=1|abnormal), one list for p(xi=0|abnormal)
     p1_normal = list()
@@ -66,6 +66,25 @@ def prepare_data_naive(p_normal, train_normal, count1):
         p0_normal = np.append(p0_normal, p_feature_0)
     return p1_normal, p0_normal
 
+# prepare data for Guassian Naive Bayes
+def prepare_data_guassian(train_normal, train_abnormal):
+    # calculate the mean for each feature
+    train_normal = train_normal.astype(np.float)
+    train_normal_mean = np.mean(train_normal, axis=0)
+
+    # calculate the standard deviation for each feature
+    train_normal_std = np.std(train_normal, axis = 0)
+    train_normal_std += 0.0001
+
+    # the same process with abnormal data
+    train_abnormal = train_abnormal.astype(np.float)
+    train_abnormal_mean = np.mean(train_abnormal, axis=0)
+    train_abnormal_std = np.std(train_abnormal, axis = 0)
+    train_abnormal_std += 0.0001
+
+    return train_normal_mean, train_normal_std, train_abnormal_mean, train_abnormal_std
+
+# load data from csv files
 def load_data(train_file_name, test_file_name):
     # create and initialize csv files' row number anf input list
     train_row_num = 0
@@ -124,8 +143,8 @@ def run_naive_bayesian(train_file_name, test_file_name):
     predicted = list()
 
     for i in range(0, test_row_num):
-        p1 = np.log(p_normal + 0.5)
-        p0 = np.log(p_abnormal + 0.5)
+        p1 = np.log(p_normal)
+        p0 = np.log(p_abnormal)
         for j in range(0,feature_num):
             if test_x_new[i][j] == 1:
                 p1 += p1_normal[j]
@@ -151,16 +170,65 @@ def run_naive_bayesian(train_file_name, test_file_name):
             true_normal += 1
     return count_correct, test_row_num, true_abnormal, count_test_abnormal, true_normal, count_test_normal
 
-def display_results(train_file_name, test_file_name, file_short_name):
+# run Guassian Naive Bayes
+def run_guassian(train_file_name, test_file_name):
+    train_y, train_row_num, train_x, test_x_new, test_row_num, test_y, count_test_normal, count_test_abnormal = load_data(train_file_name, test_file_name)
+    p_normal, p_abnormal, train_normal, train_abnormal, count1, count0 = prepare_basic_data(train_y, train_row_num, train_x)
+    train_normal_mean, train_normal_std, train_abnormal_mean, train_abnormal_std = prepare_data_guassian(train_normal, train_abnormal) 
+    p_normal_log = np.log(1/(np.sqrt(2*np.pi)*train_normal_std)) + (-np.square(test_x_new-train_normal_mean)/(2*np.square(train_normal_std)))
+    p_abnormal_log = np.log(1/(np.sqrt(2*np.pi)*train_abnormal_std)) + (-np.square(test_x_new-train_abnormal_mean)/(2*np.square(train_abnormal_std)))
+    p_normal = np.log(p_normal)
+    p_abnormal = np.log(p_abnormal)
+    p_test_normal_guassian = np.sum(p_normal_log, axis = 1) + p_normal
+    p_test_abnormal_guassian = np.sum(p_abnormal_log, axis = 1) + p_abnormal
+    predicted = np.where((p_test_normal_guassian > p_test_abnormal_guassian), 1,0)
+    count = np.count_nonzero(predicted == 0)
+    return predicted, test_y, test_x_new, test_row_num
+
+def display_results_naive(train_file_name, test_file_name, file_short_name):
     count_correct, test_row_num, true_abnormal, count_test_abnormal, true_normal, count_test_normal = run_naive_bayesian(train_file_name, test_file_name)
     accuracy = round(float(count_correct)/float(test_row_num), 2)
     true_negative = round(float(true_abnormal)/float(count_test_abnormal), 2)
     true_positive = round(float(true_normal)/float(count_test_normal), 2)
     print('{} {}/{}({}) {}/{}({}) {}/{}({})'.format(file_short_name, count_correct, test_row_num, accuracy,true_abnormal,count_test_abnormal, true_negative, true_normal, count_test_normal, true_positive))
 
+def display_results_guassian(train_file_name, test_file_name, file_short_name):
+    predicted, test_y, test_x_new, test_row_num = run_guassian(train_file_name, test_file_name)
+    count_correct = 0
+    true_abnormal = 0
+    count_test_abnormal = 0
+    true_normal = 0
+    count_test_normal = 0
+    for i in range(0, test_row_num):
+        if predicted[i] == test_y[i]:
+            count_correct += 1
+        if predicted[i] == 0 and test_y[i] == 0:
+            true_abnormal += 1
+        if test_y[i] == 0:
+            count_test_abnormal += 1
+        if predicted[i] == 1 and test_y[i] == 1:
+            true_normal += 1
+        if test_y[i] == 1:
+            count_test_normal += 1
+    accuracy = round(float(count_correct)/float(test_row_num), 2)
+    true_negative = round(float(true_abnormal)/float(count_test_abnormal), 2)
+    true_positive = round(float(true_normal)/float(count_test_normal), 2)
+    print('{} {}/{}({}) {}/{}({}) {}/{}({})'.format(file_short_name, count_correct, test_row_num, accuracy,true_abnormal,count_test_abnormal, true_negative, true_normal, count_test_normal, true_positive))
+
+
 
 # Part 1: Run Naive Bayesian
-display_results("spect-orig.train.csv", "spect-orig.test.csv", "orig")
-display_results("spect-resplit.train.csv", "spect-resplit.test.csv", "resplit")
-display_results("spect-itg.train.csv", "spect-itg.test.csv", "itg")
-display_results("spect-resplit-itg.train.csv", "spect-resplit-itg.test.csv", "resplit-itg")
+print("Naive Bayesian Learner:")
+display_results_naive("spect-orig.train.csv", "spect-orig.test.csv", "orig")
+display_results_naive("spect-resplit.train.csv", "spect-resplit.test.csv", "resplit")
+display_results_naive("spect-itg.train.csv", "spect-itg.test.csv", "itg")
+display_results_naive("spect-resplit-itg.train.csv", "spect-resplit-itg.test.csv", "resplit-itg")
+print("\n")
+
+# Part 2: Run Guassian Naive Bayes
+print("Guassian Naive Bayes Learner:")
+display_results_guassian("spect-orig.train.csv", "spect-orig.test.csv", "orig")
+display_results_guassian("spect-resplit.train.csv", "spect-resplit.test.csv", "resplit")
+display_results_guassian("spect-itg.train.csv", "spect-itg.test.csv", "itg")
+display_results_guassian("spect-resplit-itg.train.csv", "spect-resplit-itg.test.csv", "resplit-itg")
+
